225d50000d56e7dc61861426152055a59fd41ebe
Samuel Png <samuelpng7@gmail.com>
2022-08-25 09:50:26 +0000

    changes db driver

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,11 +1,15 @@
 // Setting up the database connection
 const knex = require('knex')({
-  client: 'mysql',
+  client: process.env.DB_DRIVER,
   connection: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST,
+    ssl: {
+      'rejectUnauthorized': false
+  }
+
   }
 })
 const bookshelf = require('bookshelf')(knex)
